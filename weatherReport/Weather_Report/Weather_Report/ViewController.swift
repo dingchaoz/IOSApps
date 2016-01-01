75157938aade2c96412221fd332cee195a04f85f
Dingchao Zhang <dingchaoie@gmail.com>
2016-01-01 17:40:40 -0500

    find weather app works now, need to address error when url is not valid, also the speed of getting result back seems slow

--- weatherReport/Weather_Report/Weather_Report/ViewController.swift
+++ weatherReport/Weather_Report/Weather_Report/ViewController.swift
@@ -13,11 +13,84 @@ class ViewController: UIViewController {
     @IBOutlet var userCity: UITextField!
     
     @IBAction func update(sender: AnyObject) {
+        
+        
+        // url to fetch web
+        let url = NSURL(string: "http://www.weather-forecast.com/locations/" + userCity.text!.stringByReplacingOccurrencesOfString(" ", withString: "-") + "/forecasts/latest")
+        
+        // If URL exists
+        if url != nil{
+            
+            var urlError = false
+            
+            var weather = ""
+            
+            // Initiate a task to return data, response and error
+            let task = NSURLSession.sharedSession().dataTaskWithURL(url!, completionHandler: {
+                (data,response,error) -> Void in
+                
+                // if no error returned
+                if error == nil {
+                    
+                    let urlContent = NSString(data: data!, encoding: NSUTF8StringEncoding) as NSString!
+                    
+                    let urlContentArray = urlContent.componentsSeparatedByString("<span class=\"phrase\">")
+                    
+                    if urlContentArray.count > 0 {
+                        
+                        let weatherArray = urlContentArray[1].componentsSeparatedByString("</span>")
+                        
+                        weather = weatherArray[0] as String
+                        
+                        weather = weather.stringByReplacingOccurrencesOfString("&deg;", withString: "o")
+                    }
+                    
+                    //print(urlContent)
+                }
+                else {
+                    
+                    urlError = true
+                    
+                    
+                }
+                
+                
+                if urlError == true {
+                    
+                    self.showError()
+                    
+                } else {
+                    
+                    self.result.text = weather
+                }
+                
+            })
+            
+            task.resume()
+            
+        } else {
+            
+            showError()
+            
+        }
+
+        
+        
     }
+    
+    @IBOutlet var result: UILabel!
+    
+    func showError() {
+       
+        result.text = "Was not able to find weather for" + userCity.text! + "please try again"
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-    }
+        
+           }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
