bd9fcddde8e75b1cd78b4cd4425034dbf7cf4e2e
Dingchao Zhang <dingchaoie@gmail.com>
2016-01-10 22:31:23 -0500

    start working on web view demo app

---
+++ webView/Web_View/Web_View/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Web_View
+//
+//  Created by Dingchao Zhang on 1/10/16.
+//  Copyright © 2016 Dingchao Zhang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
